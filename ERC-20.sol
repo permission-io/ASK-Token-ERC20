f4a58f5067109f64d14d45fab46ad6d4651856c9
permissionioworkspace <102548104+permissionioworkspace@users.noreply.github.com>
2022-04-14 13:20:16 -0700

    Create ERC-20.sol
    Base ERC-20 contract.

---
+++ ERC-20.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract MyToken is ERC20, ERC20Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    
+    uint256 private _maxSupply = 1e29;
+
+
+    constructor() ERC20("Permission Token", "ASK") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+     function getMaxSupply() public view returns (uint256) {
+         return _maxSupply;
+     }
+
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        require(totalSupply() + amount <= getMaxSupply(), "Max Supply Exceeded");
+        _mint(to, amount);
+    }
+}
